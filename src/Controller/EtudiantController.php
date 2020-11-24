d9d7ac378a38b4a09cd9e9b7b830719f1f75a07d
zakinaA <zakina.rostand@gmail.com>
2020-11-24 09:51:13 +0100

    premier commit

---
+++ src/Controller/EtudiantController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Etudiant;
+
+class EtudiantController extends AbstractController
+{
+    /**
+     * @Route("/etudiant", name="etudiant")
+     */
+    public function index(): Response
+    {
+        return $this->render('etudiant/index.html.twig', [
+            'controller_name' => 'EtudiantController',
+        ]);
+    }
+
+    public function accueil(): Response
+    {
+        /*return $this->render('etudiant/index.html.twig', [
+            'controller_name' => 'EtudiantController',
+        ]);*/
+        return new response('<html><body>Salut Les SIO</body></html>');
+    }
+    public function ajouterEtudiant(): Response
+    {
+      // récupère le manager d'entités
+      $entityManager = $this->getDoctrine()->getManager();
+
+      // instanciation d'un objet Etudiant
+      $etudiant = new Etudiant();
+      $etudiant->setNom('Potter');
+      $etudiant->setPrenom('Harry');
+      $etudiant->setDateNaiss(new \DateTime(date('1980-07-31')));
+
+
+      // Indique à Doctrine de persister l'objet
+      $entityManager->persist($etudiant);
+
+      // Exécue l'instruction sql permettant de persister lobjet, ici un INSERT INTO
+      $entityManager->flush();
+
+      // renvoie vers la vue de consultation de l'étudiant en passant l'objet etudiant en paramètre
+     return $this->render('etudiant/consulter.html.twig', [
+          'etudiant' => $etudiant,]);
+    }
+}
---
+++ src/Entity/Section.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\SectionRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=SectionRepository::class)
+ */
+class Section
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=5)
+     */
+    private $code;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $libelle;
+
+    /**
+     * @ORM\OneToMany(targetEntity=Etudiant::class, mappedBy="section")
+     */
+    private $etudiants;
+
+    public function __construct()
+    {
+        $this->etudiants = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getCode(): ?string
+    {
+        return $this->code;
+    }
+
+    public function setCode(string $code): self
+    {
+        $this->code = $code;
+
+        return $this;
+    }
+
+    public function getLibelle(): ?string
+    {
+        return $this->libelle;
+    }
+
+    public function setLibelle(string $libelle): self
+    {
+        $this->libelle = $libelle;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Etudiant[]
+     */
+    public function getEtudiants(): Collection
+    {
+        return $this->etudiants;
+    }
+
+    public function addEtudiant(Etudiant $etudiant): self
+    {
+        if (!$this->etudiants->contains($etudiant)) {
+            $this->etudiants[] = $etudiant;
+            $etudiant->setSection($this);
+        }
+
+        return $this;
+    }
+
+    public function removeEtudiant(Etudiant $etudiant): self
+    {
+        if ($this->etudiants->removeElement($etudiant)) {
+            // set the owning side to null (unless already changed)
+            if ($etudiant->getSection() === $this) {
+                $etudiant->setSection(null);
+            }
+        }
+
+        return $this;
+    }
+}
